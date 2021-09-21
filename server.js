7e7ee7fd151c9aac1b9e3faaef77848fd138abac
Gencko94 <ahmad94.dev@gmail.com>
2021-09-21 16:35:04 +0300

    add toggle task route

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const jsonServer = require("json-server");
 const { getTasks, createTask } = require("./controllers/tasks");
 const { getCategories } = require("./controllers/categories");
 const { activateTask } = require("./controllers/toggleTask");
-const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+
 const middlewares = jsonServer.defaults();
 //define port
 const PORT = process.env.PORT || 4000;
+
 server.use(middlewares);
 // server.use(cors());
 
 server.use(jsonServer.bodyParser);
-server.patch("/activate-task", activateTask);
 
+server.patch("/tasks", activateTask);
 server.get("/tasks", getTasks);
 server.post("/tasks", createTask);
 server.get("/categories", getCategories);
-
 server.use(router);
 
 server.listen(PORT, () => {
